76c5dedf8e093748e623821f5a2e2913ac171e14
Moary Chen <v-shilichen@microsoft.com>
2024-08-08 09:43:22 +0800

    Add managed zipkin-server

--- spring-petclinic-microservices-apphost/src/main/java/com/azure/example/petclinic/PetClinicAppHost.java
+++ spring-petclinic-microservices-apphost/src/main/java/com/azure/example/petclinic/PetClinicAppHost.java
@@ -2,8 +2,9 @@ package com.azure.example.petclinic;
 
 import com.azure.runtime.host.DistributedApplication;
 import com.azure.runtime.host.dcp.DcpAppHost;
-import com.azure.runtime.host.extensions.microservice.common.resources.ConfigServerServiceDiscovery;
+import com.azure.runtime.host.extensions.microservice.common.resources.ConfigServerService;
 import com.azure.runtime.host.extensions.microservice.common.resources.EurekaServiceDiscovery;
+import com.azure.runtime.host.extensions.microservice.common.resources.ZipkinServerService;
 import com.azure.runtime.host.extensions.spring.SpringExtension;
 
 public class PetClinicAppHost implements DcpAppHost {
@@ -18,28 +19,35 @@ public class PetClinicAppHost implements DcpAppHost {
         SpringExtension spring = app.withExtension(SpringExtension.class);
         EurekaServiceDiscovery discoveryServer = spring
             .addEurekaServiceDiscovery("eureka");
-        ConfigServerServiceDiscovery configServer = spring
+        ConfigServerService configServer = spring
             .addConfigServer("config-server")
             .withGitRepositoryPath("https://github.com/spring-petclinic/spring-petclinic-microservices-config")
+            .withExternalHttpEndpoints();
+        ZipkinServerService zipkinServer = spring
+            .addZipkinServer("zipkin-server")
             .withExternalHttpEndpoints();
 //        SpringProject configServer = spring.addSpringProject("spring-petclinic-config-server")
 //                .withExternalHttpEndpoints();
         spring.addSpringProject("spring-petclinic-customers-service")
+              .withDependency(zipkinServer)
               .withDependency(discoveryServer)
               .withDependency(configServer)
               .withReference(configServer)
               .withReference(discoveryServer);
         spring.addSpringProject("spring-petclinic-vets-service")
+              .withDependency(zipkinServer)
               .withDependency(discoveryServer)
               .withDependency(configServer)
               .withReference(configServer)
               .withReference(discoveryServer);
         spring.addSpringProject("spring-petclinic-visits-service")
+              .withDependency(zipkinServer)
               .withDependency(discoveryServer)
               .withDependency(configServer)
               .withReference(configServer)
               .withReference(discoveryServer);
         spring.addSpringProject("spring-petclinic-api-gateway")
+              .withDependency(zipkinServer)
               .withDependency(discoveryServer)
               .withDependency(configServer)
               .withReference(configServer)
